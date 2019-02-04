301b4159e4c22e0605d34f374fc6d8fbf7913bdd
matemolnar88 <matemolnar88@gmail.com>
2019-02-04 11:44:53 +0100

    Testing

--- app/config/config.php
+++ app/config/config.php
@@ -3,18 +3,17 @@
   define('DB_HOST', 'localhost');
   define('DB_USER', 'root');
   define('DB_PASS', '');
-  define('DB_NAME', 'shareposts');
+  define('DB_NAME', 'booklet');
 
   // App root
   define('APPROOT', dirname(dirname(__FILE__)));
 
   // URL Root
-  // define('URLROOT', 'http://localhost/Git/HypertextPreProcessor_PHP/BlogPostPhp/shareposts');
-  define('URLROOT', 'http://localhost/Git/HypertextPreProcessor_PHP/BlogPostPhp/shareposts');
+  define('URLROOT', 'http://localhost/Git/x_DevCornerBooklet');
 
   // Site Name
   // define('SITENAME', 'mate_mvc');
-  define('SITENAME', 'SharePosts');
+  define('SITENAME', 'DevCornerBooklet');
 
   // App version
   define('APPVERSION', '1.0.0');
