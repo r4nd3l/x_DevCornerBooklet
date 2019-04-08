7d4456e3e7ffd79d6261573776fd4e9ccc37878e
matemolnar88 <matemolnar88@gmail.com>
2019-04-08 16:53:14 +0200

    First commit

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -81,23 +81,3 @@
        return $this->stmt->rowCount();
      }
    }
-
-
-
-          // sql to create table - posts
-          // $sql = "CREATE TABLE posts (
-          // id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-          // user_id int(11) NOT NULL,
-          // title varchar(255) NOT NULL,
-          // body text NOT NULL,
-          // created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-          // )";
-
-          // sql to create table - users
-          // $sql = "CREATE TABLE users (
-          // id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-          // name varchar(255) NOT NULL,
-          // email varchar(255) NOT NULL,
-          // password varchar(255) NOT NULL,
-          // created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-          // )";
--- readme.md
+++ readme.md
@@ -1,4 +1,11 @@
 Html, Css and Php based Responsive Blogging website - exercise
 ---
 
-![BlogPost](https://github.com/r4nd3l/BlogPost/blob/master/public/img/sample.png)
+![BlogPost](https://github.com/r4nd3l/BlogPost/blob/master/public/img/sample.gif)
+
+- Overwrite these files paths: (accoring to your system)
+  - app/config/config.php
+      define('URLROOT', 'http://localhost/Git/x_DevCornerBooklet');
+  - public/.htaccess
+      "RewriteBase /Git/x_DevCornerBooklet/public"
+- Create the database in PhpMyAdmin and /db_init.php (in the URL) will do the rest
